b1075e81c32e2b75c39108f74f9a1b8ed0756c18
Ashish Bhosle <ashish.bhosle008@gmail.com>
2019-09-20 19:16:28 +0530

    Used reduce to get max for each date

--- problem-statement2/src/main/java/assignments/kafka/service/KafkaStreamService.java
+++ problem-statement2/src/main/java/assignments/kafka/service/KafkaStreamService.java
@@ -1,31 +1,19 @@
 package assignments.kafka.service;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Locale;
 import java.util.Properties;
-import java.util.Set;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
-import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.Materialized;
-import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.kstream.KeyValueMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.fasterxml.jackson.databind.MappingIterator;
 
 import assignments.kafka.common.AppUtils;
 import assignments.kafka.common.serialization.StockTradeDeserializer;
@@ -60,10 +48,18 @@ public class KafkaStreamService {
 		KStream<String, StockTrade> source = builder.stream(topic, Consumed.with(Serdes.String(),
 				Serdes.serdeFrom(new StockTradeSerializer(), new StockTradeDeserializer())));
 
-		source.groupBy((k, v) -> v.getTimestamp()).count().toStream().to(topic + "-out");
-
 //		source.groupBy((k, v) -> v.getTimestamp()).count().toStream().to(topic + "-out",
 //				Produced.with(Serdes.String(), Serdes.Long()));
+
+		// source.groupBy((k, v) -> KeyValue.pair(k, v.getTotTrdVal()),
+		// Produced.with(Serdes.String(), Serdes.Double())).aggregate(initializer,
+		// adder, subtractor);
+
+		source.map((k, v) -> KeyValue.pair(k, v.getTotTrdVal())).groupByKey()
+				.reduce((aggVal, newVal) -> aggVal < newVal ? newVal : aggVal).toStream().to(topic + "-out");
+
+//		source.mapValues(StockTrade::getTotTrdVal).groupByKey()
+//				.reduce((aggVal, newVal) -> aggVal < newVal ? newVal : aggVal).toStream().to(topic + "-out");
 
 		final Topology topology = builder.build();
 		final KafkaStreams streams = new KafkaStreams(topology, configs());
@@ -102,7 +98,7 @@ public class KafkaStreamService {
 		props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG,
 				kp.getKafkaStreams().get(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG));
 		props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-		props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+		props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.Double().getClass());
 
 		return props;
 	}
